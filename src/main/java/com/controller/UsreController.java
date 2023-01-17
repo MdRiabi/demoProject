3ee2fc4940b82a292364eda9dd4d7f155116daf9
MdRiabi <mhdriabi2013@gmail.com>
2023-01-17 21:51:08 +0100

    implement findbyid in the controller

--- src/main/java/com/controller/UsreController.java
+++ src/main/java/com/controller/UsreController.java
@@ -1,8 +1,11 @@
 package com.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,16 +13,25 @@ import com.model.User;
 import com.service.UserService;
 
 @RestController
-@RequestMapping("/api/user")
+@RequestMapping("/api/users")
 public class UsreController {
 	
 	@Autowired
 	private UserService userService;
-	
-	
+	@GetMapping
 	public List<User> findAll(){
 		
 		return userService.findAll();
 	}
+
+
+    @GetMapping("/{id}")
+    public  User findById(@PathVariable Long id){
+        Optional<User> userOpt = userService.findById(id);
+        if (userOpt.isPresent()){
+            return  userOpt.get();
+        }
+        return null;
+    }
 
 }
