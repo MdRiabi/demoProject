0ea2a68b43c6fedf4c5fac4e6d34330f13a97d1c
MdRiabi <mhdriabi2013@gmail.com>
2023-01-17 21:50:36 +0100

    implement findbyid in the service

--- src/main/java/com/service/Impl/UserServiceImpl.java
+++ src/main/java/com/service/Impl/UserServiceImpl.java
@@ -2,6 +2,7 @@ package com.service.Impl;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
@@ -14,24 +15,22 @@ public class UserServiceImpl implements UserService {
 	
 	
 	public static List<User> userList = new ArrayList<>();
-	public static Integer COUNTER = 0;
+	public static long COUNTER ;
 	
 	static {
 		User user = new User(COUNTER++, "Med","riabi","32","tunis");
 		userList.add(user);
-		
-		new User(COUNTER++, "zina","ferchichi","50","tunis");
-		userList.add(user);
-		
-		new User(COUNTER++, "tarek","riabi","33","tunis");
-		userList.add(user);
-		
-		new User(COUNTER++, "hamza","riabi","25","tunis");
-		userList.add(user);
+
+		User user1 =new User(COUNTER++, "zina","ferchichi","50","tunis");
+		userList.add(user1);
+
+		User user2 =new User(COUNTER++, "tarek","riabi","33","tunis");
+		userList.add(user2);
+
+		User user3 =new User(COUNTER++, "hamza","riabi","25","tunis");
+		userList.add(user3);
 		
 	}
-	
-	
 
 	@Override
 	public List<User> findAll() {
@@ -39,4 +38,14 @@ public class UserServiceImpl implements UserService {
 		return userList;
 	}
 
+	@Override
+	public Optional<User> findById(Long id) {
+
+		return   userList.stream().filter(user -> user.getId() == id).findFirst();
+
+//		 if (userOpt.isPresent()){
+//			return  userOpt;
+//		 }
+//		 return  Optional.empty();
+	}
 }
