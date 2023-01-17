153d3129e31c4df6f1edb2b02f7ed409a71701b7
MdRiabi <mhdriabi2013@gmail.com>
2023-01-17 19:34:32 +0100

    update user class

--- src/main/java/com/model/User.java
+++ src/main/java/com/model/User.java
@@ -1,5 +1,169 @@
 package com.model;
 
+import java.util.Objects;
+
 public class User {
+	private Integer id;
+	private String firstName;
+	private String lastName;
+	private String age;
+	private String country;
+	
+	
+	public User() {
+		super();
+	}
+	
+
+	
+	
+	
+
+
+
+
+
+
+	public User(Integer id, String firstName, String lastName, String age, String country) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.age = age;
+		this.country = country;
+	}
+
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+
+	public String getLastName() {
+		return lastName;
+	}
+
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+
+	public String getAge() {
+		return age;
+	}
+
+
+	public void setAge(String age) {
+		this.age = age;
+	}
+
+
+	public String getCountry() {
+		return country;
+	}
+
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+
+
+	public Integer getId() {
+		return id;
+	}
+
+
+
+
+
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	
+
+
+
+
+
+
+
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(age, country, firstName, id, lastName);
+	}
+
+
+
+
+
+
+
+
+
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Objects.equals(age, other.age) && Objects.equals(country, other.country)
+				&& Objects.equals(firstName, other.firstName) && Objects.equals(id, other.id)
+				&& Objects.equals(lastName, other.lastName);
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", age=" + age + ", country="
+				+ country + "]";
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
