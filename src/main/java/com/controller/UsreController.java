e4355af27644c5db39ad9d8d33877ad295a22fc4
MdRiabi <mhdriabi2013@gmail.com>
2023-01-17 23:32:32 +0100

    implement add and delete methods in the controller

--- src/main/java/com/controller/UsreController.java
+++ src/main/java/com/controller/UsreController.java
@@ -4,13 +4,14 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import com.model.User;
 import com.service.UserService;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/users")
@@ -19,19 +20,38 @@ public class UsreController {
 	@Autowired
 	private UserService userService;
 	@GetMapping
-	public List<User> findAll(){
+	public ResponseEntity<?> findAll(){
 		
-		return userService.findAll();
+		return new ResponseEntity<List<User>>(userService.findAll() , HttpStatus.OK);
 	}
 
 
     @GetMapping("/{id}")
-    public  User findById(@PathVariable Long id){
+    public ResponseEntity<?> findById(@PathVariable Long id){
         Optional<User> userOpt = userService.findById(id);
         if (userOpt.isPresent()){
-            return  userOpt.get();
+            return  new ResponseEntity<User>(userOpt.get() , HttpStatus.OK);
         }
-        return null;
+        return new ResponseEntity<Void>(HttpStatus.NOT_FOUND);
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<?> add(@Valid @RequestBody User user){
+
+        userService.add(user);
+        return  new ResponseEntity<Void>(HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id){
+
+        Optional<User> optUsre = userService.delete(id);
+
+        if (optUsre.isPresent()){
+            return  new ResponseEntity<User>(optUsre.get(), HttpStatus.NO_CONTENT);
+
+        }
+        return new ResponseEntity<Void>(HttpStatus.NOT_FOUND);
     }
 
 }
