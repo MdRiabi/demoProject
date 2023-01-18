733fbee72192c6e518180b045eeba2dae6b83724
MdRiabi <mhdriabi2013@gmail.com>
2023-01-18 23:44:32 +0100

    load list of users in each app run

---
+++ src/main/java/com/component/LoadUsersInDB.java
@@ -0,0 +1,53 @@
+package com.component;
+
+import com.model.User;
+import com.repository.UserRepository;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+@Transactional
+public class LoadUsersInDB  implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository userRepository;
+    
+    @Autowired
+     private PasswordEncoder passwordEncoder ;
+
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        User user0 = new User("louay", UUID.randomUUID().toString() , "Med", "riabi", "23" , "tunis");
+        User user1 = new User("userA", UUID.randomUUID().toString() , "zina", "ferchichi", "50", "tunis");
+        User user2 = new User("userB", UUID.randomUUID().toString() ,  "tarek", "riabi", "33", "tunis");
+        User user3 = new User("userC", UUID.randomUUID().toString() ,  "hamza", "riabi", "25", "tunis");
+        User user4 = new User("userD", UUID.randomUUID().toString() , "zina", "ferchichi", "50", "tunis");
+        User user5 = new User( "userE", UUID.randomUUID().toString() , "tarek", "riabi", "33", "tunis");
+        User user6 = new User( "userF", UUID.randomUUID().toString() , "hamza", "riabi", "25", "tunis");
+        User user7 = new User("userG", UUID.randomUUID().toString() , "zina", "ferchichi", "50", "tunis");
+        User user8 = new User("userH", UUID.randomUUID().toString() ,  "tarek", "riabi", "33", "tunis");
+        User user9 = new User("userI", UUID.randomUUID().toString() ,  "hamza", "riabi", "25", "tunis");
+        
+        List<User> usersList = Arrays.asList(user0, user1, user2 , user3 , user4, user5, user6, user7, user8 , user9);
+        
+        usersList = usersList.stream().map(user -> {
+        	user.setPassword(passwordEncoder.encode(user.getPassword()));
+        	return user;
+        }).collect(Collectors.toList());
+        
+        
+        userRepository.saveAll(usersList);
+    }
+}
