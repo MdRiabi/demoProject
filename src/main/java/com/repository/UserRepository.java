ee9304f1e3114d6a0a27187cd4db4338e1b3e3bd
MdRiabi <mhdriabi2013@gmail.com>
2023-01-19 16:24:07 +0100

    add same methods in repo

--- src/main/java/com/repository/UserRepository.java
+++ src/main/java/com/repository/UserRepository.java
@@ -1,11 +1,21 @@
 package com.repository;
 
 import com.model.User;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+	
+	public List<User> findByUserName(String username);
+	public List<User> findByFirstName(String firstName);
+	public List<User> findByLastName(String lastName);
+	public List<User> findByAge(String age);
+	public List<User> findByCountry(String country);
+
 
 
 
