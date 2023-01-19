fb2b4fb2c97facc744c756cb56c26a8654278dc1
MdRiabi <mhdriabi2013@gmail.com>
2023-01-19 16:24:27 +0100

    add same methods in repo

--- src/main/java/com/service/UserService.java
+++ src/main/java/com/service/UserService.java
@@ -3,13 +3,16 @@
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import com.model.User;
 
 
   public interface UserService {
 	
 	
-	public List<User> findAll();
+	public Page<User> findAll(Pageable pageable);
 
 	public Optional<User> findById(Long id);
 
@@ -20,5 +23,6 @@ import com.model.User;
 
 	public Optional<User> update(User user);
 
+	public List<User> findByCriteria(String criteria , String searchItem );
 
 }
